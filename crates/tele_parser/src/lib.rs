7feed2f487c014db41329c4ca5bed44e0176494b
HcySunYang <HcySunYang@outlook.com>
2022-01-28 20:50:47 +0800

    wip: consume tokens

--- crates/tele_parser/src/lib.rs
+++ crates/tele_parser/src/lib.rs
@@ -4,20 +4,36 @@
 
 //! CSS Parser
 
+use std::{
+  cell::RefCell,
+  iter::{Enumerate, Peekable},
+  slice::Iter,
+};
+
 use tele_tokenizer::*;
 
 pub struct Parser<'s> {
   tokens: &'s Vec<Token<'s>>,
+  iter: RefCell<Peekable<Enumerate<Iter<'s, Token<'s>>>>>,
 }
 
 impl<'s> From<&'s Vec<Token<'s>>> for Parser<'s> {
   fn from(tokens: &'s Vec<Token<'s>>) -> Self {
-    Self { tokens }
+    Self {
+      tokens,
+      iter: RefCell::new(tokens.iter().enumerate().peekable()),
+    }
   }
 }
 
 impl<'s> Parser<'s> {
   pub fn parse(&self) {
-    println!("{:#?}", self.tokens);
+    let mut iter = self.iter.borrow_mut();
+    while let Some(&(offset, _token)) = iter.peek() {
+      println!("offset: {}", offset);
+      println!("Token: {:#?}", self.tokens[offset]);
+
+      iter.next();
+    }
   }
 }
