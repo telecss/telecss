dbe2508b0f9f44fafe920091bae93fce686e3571
github-action-benchmark <github@users.noreply.github.com>
2022-02-08 12:26:58 +0000

    add Rust Benchmark (cargo) benchmark result for 62efa8030ecab8f7f604a7e26eb48ec26ccf6332

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1644323218655,
+  "repoUrl": "https://github.com/telecss/telecss",
+  "entries": {
+    "Rust Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "HcySunYang@outlook.com",
+            "name": "HcySunYang",
+            "username": "HcySunYang"
+          },
+          "committer": {
+            "email": "HcySunYang@outlook.com",
+            "name": "HcySunYang",
+            "username": "HcySunYang"
+          },
+          "distinct": true,
+          "id": "62efa8030ecab8f7f604a7e26eb48ec26ccf6332",
+          "message": "ci: fix output file path",
+          "timestamp": "2022-02-08T20:10:24+08:00",
+          "tree_id": "15a4c8efbfe4c69806f7a11a856dc18809e70c6c",
+          "url": "https://github.com/telecss/telecss/commit/62efa8030ecab8f7f604a7e26eb48ec26ccf6332"
+        },
+        "date": 1644323217952,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "tests::parse_normalize_css",
+            "value": 33647,
+            "range": "± 2600",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
