8aea2eb3462240922ec8fa8836687e734b9bd446
HcySunYang <HcySunYang@outlook.com>
2022-02-18 09:56:20 +0800

    tests: rename test

--- crates/tele_folder/tests/folder.rs
+++ crates/tele_folder/tests/folder.rs
@@ -6,7 +6,7 @@ use tele_tokenizer::Tokenizer;
 struct Renamer;
 impl Folder for Renamer {
   fn fold_decl_node(&mut self, mut decl_node: DeclarationNode) -> DeclarationNode {
-    decl_node.name = format!("prefix-{}", decl_node.name);
+    decl_node.name = format!("-moz-{}", decl_node.name);
     decl_node
   }
 }
@@ -16,7 +16,7 @@ fn transform<T: Folder>(ast: StyleSheetNode, mut folder: T) -> StyleSheetNode {
 }
 
 #[test]
-fn ruleset_with_a_single_decl() {
+fn test_fold_decl_node() {
   let mut tokenizer: Tokenizer = r"  .foo { color: red; }  ".into();
   let parser = Parser::from(tokenizer.tokenize().unwrap());
   assert_debug_snapshot!(transform(parser.parse().unwrap(), Renamer));
