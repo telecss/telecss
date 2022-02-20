ef916c9c06391992d62d50ed460aecfa235589d8
HcySunYang <HcySunYang@outlook.com>
2022-02-20 14:56:24 +0800

    docs: add docs for AST Node

--- crates/tele_parser/src/node.rs
+++ crates/tele_parser/src/node.rs
@@ -12,7 +12,7 @@ pub struct Loc {
 #[derive(Debug, Default, PartialEq)]
 /// Represents a CSS declaration
 pub struct DeclarationNode<'s> {
-  /// Location information
+  /// Location information in the source file
   pub loc: Loc,
   /// Decalration name
   pub name: String,
@@ -27,39 +27,62 @@ pub struct DeclarationNode<'s> {
 }
 
 #[derive(Debug, Default, PartialEq)]
+/// Represents a ruleset
 pub struct RuleSetNode<'s> {
+  /// Location information in the source file
   pub loc: Loc,
+  /// prelude of the ruleset
   pub prelude: String,
+  /// The tokens that made up the prelude of the ruleset
   pub prelude_tokens: Vec<&'s Token<'s>>,
+  /// All declarations of the ruleset
   pub declarations: Vec<DeclarationNode<'s>>,
 }
 
 #[derive(Debug, Default, PartialEq)]
+/// Represents a at-rule
 pub struct AtRuleNode<'s> {
+  /// Location information in the source file
   pub loc: Loc,
+  /// The name of the at-rule
   pub name: String,
+  /// The tokens that made up the name of the at-rule
   pub name_tokens: Vec<&'s Token<'s>>,
+  /// prelude of the at-rule
   pub prelude: String,
+  /// The tokens that made up the prelude of the at-rule
   pub prelude_tokens: Vec<&'s Token<'s>>,
+  /// The block of the at-rule, consists of statements
   pub block: Vec<StatementNode<'s>>,
 }
 
 #[derive(Debug, PartialEq)]
+/// A statement is either a ruleset or an at-rule
 pub enum StatementNode<'s> {
+  /// Contains a ruleset node
   RuleSet(RuleSetNode<'s>),
+  /// Contains a at-rule node
   AtRule(AtRuleNode<'s>),
 }
 
 #[derive(Debug, Default, PartialEq)]
+/// Represents a stylesheet
 pub struct StyleSheetNode<'s> {
+  /// Location information in the source file
   pub loc: Loc,
+  /// All statements of the stylesheet
   pub statements: Vec<StatementNode<'s>>,
 }
 
 #[derive(Debug, PartialEq)]
+/// The root node of an AST
 pub enum ASTNode<'s> {
+  /// Contains a stylesheet node
   StyleSheet(StyleSheetNode<'s>),
+  /// Contains a ruleset node
   RuleSet(RuleSetNode<'s>),
+  /// Contains a at-rule node
   AtRule(AtRuleNode<'s>),
+  /// Contains a declaration node
   Declaration(DeclarationNode<'s>),
 }
