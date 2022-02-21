dff5e6f58d1e51cbc2fff58f36c19611759dc290
HcySunYang <HcySunYang@outlook.com>
2022-02-21 17:55:20 +0800

    refactor: refactoring transformer using Rc and RefCell

--- crates/tele_visit/tests/visit_mut.rs
+++ crates/tele_visit/tests/visit_mut.rs
@@ -1,19 +1,28 @@
 use insta::assert_debug_snapshot;
+use std::{cell::RefCell, rc::Rc};
 use tele_parser::{DeclarationNode, Parser};
 use tele_tokenizer::Tokenizer;
-use tele_visit::{transform, VisitMut};
+use tele_visit::{MutVisitor, VisitMut};
 
 struct Renamer;
-impl<'s> VisitMut<'s> for Renamer {
-  fn visit_decl_node(&mut self, mut decl_node: DeclarationNode<'s>) -> DeclarationNode<'s> {
+impl<'s> MutVisitor<'s> for Renamer {
+  fn visit_decl_node(&self, decl_node: Rc<RefCell<DeclarationNode<'s>>>) {
+    let mut decl_node = decl_node.borrow_mut();
     decl_node.name = format!("-moz-{}", decl_node.name);
-    decl_node
   }
 }
 
 #[test]
 fn test_visit_decl_node() {
+  // tokenizing
   let mut tokenizer: Tokenizer = r"  .foo { color: red; }  ".into();
-  let parser = Parser::from(tokenizer.tokenize().unwrap());
-  assert_debug_snapshot!(transform(parser.parse().unwrap(), Renamer));
+  let tokens = tokenizer.tokenize().unwrap();
+  // parsing
+  let parser = Parser::from(tokens);
+  let ast = Rc::new(RefCell::new(parser.parse().unwrap()));
+  // transforming
+  let transformer = VisitMut::new(Rc::clone(&ast), vec![Renamer]);
+  transformer.transform();
+
+  assert_debug_snapshot!(ast);
 }
